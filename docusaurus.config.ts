3ec07aa59a2b53c0af70da287c76de8ec662ccb7
xzsajo <1406665311@qq.com>
2025-09-29 05:20:48 +0800

    去掉url中的'/docs/'

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
 	favicon: "images/favicon.png",
 
 	// Set the production url of your site here
-	url: "https://xzsajo0314.github.io/docs/",
+	url: "https://xzsajo0314.github.io",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: "/docs/",
